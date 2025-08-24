a77e396b1c2003cd683e616200b1560ac0943f55
Daham Sathmina <sathminaoshada12@gmail.com>
2025-08-24 17:02:00 +0530

    Test Update

--- backend/app/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-from pythonjsonlogger import jsonlogger
-
-logger = logging.getLogger("ods")
-handler = logging.StreamHandler()
-formatter = jsonlogger.JsonFormatter('%(asctime)s %(levelname)s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
--- backend/app/services/embeddings.py
+++
@@ -1,7 +0,0 @@
-from .llm_provider import OllamaProvider
-
-provider = OllamaProvider()
-
-def get_embedding(text: str):
-    emb = provider.embed(text)
-    return emb
